64e8b299e9c130e8b7acb8592328bf8a98f57532
rockwolf <thereisanewway@gmail.com>
2012-03-13 14:17:33 +0100

    I don't want null in my database!

--- tj/sql/create_tables.sql
+++ tj/sql/create_tables.sql
@@ -4,19 +4,19 @@ BEGIN;
 CREATE TABLE T_TRADING_JOURNAL
 (
     tjid serial not null,
-    date timestamp not null,
+    date timestamp not null default current_date,
     sid int not null,
     long_flag char not null default 'L',
-    quantity decimal(18,4) default 0,
-    bought decimal(18,4) default 0,
-    sold decimal(18,4) default 0,
-    profit decimal(18,4) default 0,
+    quantity decimal(18,4) not null default 0,
+    bought decimal(18,4) not null default 0,
+    sold decimal(18,4) not null default 0,
+    profit decimal(18,4) not null default 0,
     win int not null default 0,
     win_sum int not null default 0,
-    win_proc decimal(18,4) default 0,
+    win_proc decimal(18,4) not null default 0,
     processed int not null default 0,
-    date_create timestamp,
-    date_modify timestamp,
+    date_create timestamp not null default current_date,
+    date_modify timestamp not null default current_date,
     constraint pk_tjid primary key(tjid),
     constraint fk_sid foreign key(sid) references T_STOCK(sid)
 );
