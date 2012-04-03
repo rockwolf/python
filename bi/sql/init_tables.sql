ca1bda2eb8c7017f78577f1558c6145404058e97
rockwolf <thereisanewway@gmail.com>
2012-04-03 10:23:37 +0300

    Budget implementation changes - initialize T_BUDGET properly + changed idea to use year and month, which will be easier to use in reports and the application.

--- bi/sql/init_tables.sql
+++ bi/sql/init_tables.sql
@@ -22,59 +22,58 @@ values(
 );
 
 /* T_BUDGET */
--- counter = 1, which means the first month
--- we can select max(counter), so no other months need to be specified
+-- we can select max, so no other months need to be specified
 -- if the budget remains the same for all months
-insert into T_BUDGET(bnid, bcid, counter, amount, active, date_created, date_modified)
-values(1, 2, 1, 0.0, 0, current_date, current_date); -- account
+insert into T_BUDGET(bnid, bcid, year, month, amount, active, date_created, date_modified)
+values(1, 2, 2012, 1, 0.0, 0, current_date, current_date); -- account
 
-insert into T_BUDGET(bnid, bcid, counter, amount, active, date_created, date_modified)
-values(1, 4, 1, 0.0, 0, current_date, current_date); -- bet
+insert into T_BUDGET(bnid, bcid, year, month, amount, active, date_created, date_modified)
+values(1, 4, 2012, 1, 0.0, 0, current_date, current_date); -- bet
 
-insert into T_BUDGET(bnid, bcid, counter, amount, active, date_created, date_modified)
-values(1, 6, 1, 0.0, 1, current_date, current_date); -- bill
+insert into T_BUDGET(bnid, bcid, year, month, amount, active, date_created, date_modified)
+values(1, 6, 2012, 1, 50.0, 1, current_date, current_date); -- bill
 
-insert into T_BUDGET(bnid, bcid, counter, amount, active, date_created, date_modified)
-values(1, 8, 1, 0.0, 1, current_date, current_date); -- car
+insert into T_BUDGET(bnid, bcid, year, month, amount, active, date_created, date_modified)
+values(1, 8, 2012, 1, 195.83, 1, current_date, current_date); -- car
 
-insert into T_BUDGET(bnid, bcid, counter, amount, active, date_created, date_modified)
-values(1, 10, 1, 0.0, 1, current_date, current_date); -- clothes
+insert into T_BUDGET(bnid, bcid, year, month, amount, active, date_created, date_modified)
+values(1, 10, 2012, 1, 20.0, 1, current_date, current_date); -- clothes
 
-insert into T_BUDGET(bnid, bcid, counter, amount, date_created, date_modified)
-values(1, 12, 1, 50.0, 1, current_date, current_date); -- extra
+insert into T_BUDGET(bnid, bcid, year, month, amount, date_created, date_modified)
+values(1, 12, 2012, 1, 50.0, 1, current_date, current_date); -- extra
 
-insert into T_BUDGET(bnid, bcid, counter, amount, active, date_created, date_modified)
-values(1, 14, 1, 0.0, 1, current_date, current_date); -- food
+insert into T_BUDGET(bnid, bcid, year, month, amount, active, date_created, date_modified)
+values(1, 14, 2012, 1, 150.0, 1, current_date, current_date); -- food
 
-insert into T_BUDGET(bnid, bcid, counter, amount, active, date_created, date_modified)
-values(1, 16, 1, 0.0, 1, current_date, current_date); -- gift
+insert into T_BUDGET(bnid, bcid, year, month, amount, active, date_created, date_modified)
+values(1, 16, 2012, 1, 25.0, 1, current_date, current_date); -- gift
 
-insert into T_BUDGET(bnid, bcid, counter, amount, active, date_created, date_modified)
-values(1, 18, 1, 0.0, 0, current_date, current_date); -- hobby
+insert into T_BUDGET(bnid, bcid, year, month, amount, active, date_created, date_modified)
+values(1, 18, 2012, 1, 50.0, 1, current_date, current_date); -- hobby
 
-insert into T_BUDGET(bnid, bcid, counter, amount, active, date_created, date_modified)
-values(1, 20, 1, 0.0, 0, current_date, current_date); -- house
+insert into T_BUDGET(bnid, bcid, year, month, amount, active, date_created, date_modified)
+values(1, 20, 2012, 1, 600.0, 1, current_date, current_date); -- house
 
-insert into T_BUDGET(bnid, bcid, counter, amount, active, date_created, date_modified)
-values(1, 22, 1, 0.0, 0, current_date, current_date); -- invest
+insert into T_BUDGET(bnid, bcid, year, month, amount, active, date_created, date_modified)
+values(1, 22, 2012, 1, 0.0, 0, current_date, current_date); -- invest
 
-insert into T_BUDGET(bnid, bcid, counter, amount, active, date_created, date_modified)
-values(1, 24, 1, 0.0, 0, current_date, current_date); -- trade
+insert into T_BUDGET(bnid, bcid, year, month, amount, active, date_created, date_modified)
+values(1, 24, 2012, 1, 0.0, 0, current_date, current_date); -- trade
 
-insert into T_BUDGET(bnid, bcid, counter, amount, active, date_created, date_modified)
-values(1, 26, 1, 0.0, 0, current_date, current_date); -- salary
+insert into T_BUDGET(bnid, bcid, year, month, amount, active, date_created, date_modified)
+values(1, 26, 2012, 1, 0.0, 0, current_date, current_date); -- salary
 
-insert into T_BUDGET(bnid, bcid, counter, amount, active, date_created, date_modified)
-values(1, 28, 1, 0.0, 0, current_date, current_date); -- tax
+insert into T_BUDGET(bnid, bcid, year, month, amount, active, date_created, date_modified)
+values(1, 28, 2012, 1, 0.0, 0, current_date, current_date); -- tax
 
-insert into T_BUDGET(bnid, bcid, counter, amount, active, date_created, date_modified)
-values(1, 30, 1, 0.0, 1, current_date, current_date); -- travel
+insert into T_BUDGET(bnid, bcid, year, month, amount, active, date_created, date_modified)
+values(1, 30, 2012, 1, 66.67, 1, current_date, current_date); -- travel
 
-insert into T_BUDGET(bnid, bcid, counter, amount, active, date_created, date_modified)
-values(1, 32, 1, 0.0, 1, current_date, current_date); -- utilities
+insert into T_BUDGET(bnid, bcid, year, month, amount, active, date_created, date_modified)
+values(1, 32, 2012, 1, 150.0, 1, current_date, current_date); -- utilities
 
-insert into T_BUDGET(bnid, bcid, counter, amount, active, date_created, date_modified)
-values(1, 34, 1, 0.0, 1, current_date, current_date); -- other
+insert into T_BUDGET(bnid, bcid, year, month, amount, active, date_created, date_modified)
+values(1, 34, 2012, 1, 0.0, 1, current_date, current_date); -- other
 
 /* T_ALLOCATION_CAPITAL */
 insert into T_ALLOCATION_CAPITAL(allocation_proc, description, date_created, date_modified)
