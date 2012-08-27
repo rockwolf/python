0d94188ee75ab50e37ef9d1c9ce1a44d21f0e073
Andy Nagels <thereisanewway@gmail.com>
2012-08-27 21:13:33 +0200

    Removing table items and readding the ones I defined. Also removed the persistency, because that's not gonna work in practice. Just write the data to txt-files to remember them, but treat each page seperately.

--- emma/pyqt/controllerpyqt.py
+++ emma/pyqt/controllerpyqt.py
@@ -64,12 +64,50 @@ class ControllerPyqt(QtGui.QMainWindow):
         # TODO: set header values in mdlconstants and use the constants
         header = ['total', 'defense', 'offense', 'invest']
         self.table_division = TableModel(header, [], 0, len(header))
+        self.gui.grd_tables.addWidget(self.table_division, 1, 0)
+        self.table_division.add_row(['test1', 'test2', 'test3', 'test4'])
+
+    def init_tbl_set_trading(self):
+        """ Initialize table division. """
+        # set the table header
+        # TODO: set header values in mdlconstants and use the constants
+        header = ['total', 'defense', 'offense', 'invest']
+        self.table_set_trading = TableModel(header, [], 0, len(header))
+        self.gui.grd_tables.addWidget(self.table_set_trading, 5, 3)
+        self.table_set_trading.add_row(['test1', 'test2', 'test3', 'test4'])
+
+    def print_objects_in_grid(self):
+        """ Used for testing. """
+        # The below code prints the objects in the grid.
+        print('Per row/column:')
+        print('---------------')
+        rows = self.gui.grd_tables.rowCount()
+        columns = self.gui.grd_tables.columnCount()
+        for i in range(rows):
+            for j in range(columns):
+                print(self.gui.grd_tables.itemAtPosition(i,j), i, j)
+        print('Per item:')
+        print('---------')
         for i in range(self.gui.grd_tables.count()):
-            print(self.gui.grd_tables.itemAt(i), i, self.gui.grd_tables.itemAt(i).widget())
-        #self.gui.grd_tables.takeAt(0)
-        #self.gui.grd_tables.addWidget(self.table_division)
+            print(self.gui.grd_tables.itemAt(i), i,
+                    self.gui.grd_tables.itemAt(i).widget())
+
+    def delete_table_widgets(self, gridlayout):
+        """ Deletes table widgets at a certain index in a given gridlayout. """
+        print('Delete shit')
+        print('-----------')
+        for i in range(gridlayout.count()):
+            print(i)
+            if gridlayout.itemAt(i) is not None:
+                if isinstance(gridlayout.itemAt(i).widget(), QtGui.QTableWidget):
+                    self.gui.grd_tables.takeAt(i)
 
     def init_gui(self):
         """ Initialise fields """
+        self.gui.tab_main.setCurrentIndex(4)
+        self.print_objects_in_grid()
+        # Delete tables
+        self.delete_table_widgets(self.gui.grd_tables)
         # Init tables
         self.init_tbl_division()
+        #self.init_tbl_set_trading()
