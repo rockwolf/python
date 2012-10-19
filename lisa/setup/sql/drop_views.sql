6a5e94ea307dd58a8083235db23ba2cfa0c10fe8
rockwolf <thereisanewway@gmail.com>
2012-10-19 17:00:53 +0300

    Update lisa/setup/sql/drop_views.sql
    T_STOCK no longer necessary - part 10.

--- lisa/setup/sql/drop_views.sql
+++ lisa/setup/sql/drop_views.sql
@@ -3,7 +3,7 @@ drop view V_FINANCE;
 drop view V_SUBCATEGORY;
 drop view V_CATEGORY;
 drop view V_ACCOUNT;
-drop view V_STOCK;
+drop view V_INVESTMENT;
 drop view V_STOCK_NAME;
 drop view V_MARKET;
 drop view V_MARGIN_TYPE;
