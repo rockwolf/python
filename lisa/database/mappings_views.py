03d6fdf40af97dedf45325fa102ebf8d38dea276
Andy Nagels <thereisanewway@gmail.com>
2012-10-21 17:43:49 +0200

    Fixed the view mappings and tested the writing of T_CURRENCY_EXCHANGE. It's good.

--- lisa/database/mappings_views.py
+++ lisa/database/mappings_views.py
@@ -18,43 +18,46 @@ along with Lisa. If not, see <http://www.gnu.org/licenses/>.
 class V_FINANCE(object):
     """ V_FINANCE """
 
-    def __init__(self, date, account_id, category_id, subcategory_id,
-            amount, comment, stock,
-            market, shares, price, tax, commission, active, date_created,
-            date_modified, risk, year, month, day):
+    def __init__(self, finance_id, date, year, month, day, account_id, category_id,
+            subcategory_id, amount, comment, stock_name_id, shares, price, tax,
+            commission, active, rate_id, currency_exchange_id, date_created, date_modified):
+        self.finance_id = finance_id
         self.date = date
+        self.year = year
+        self.month = month
+        self.day = day
         self.account_id = account_id
         self.category_id = category_id 
         self.subcategory_id = subcategory_id 
         self.amount = amount
         self.comment = comment
-        self.stock = stock
-        self.market = market
+        self.stock_name_id = stock_name_id
         self.shares = shares
         self.price = price
         self.tax = tax
         self.commission = commission
         self.active = active
+        self.rate_id = rate_id 
+        self.currency_exchange_id = currency_exchange_id
         self.date_created = date_created
         self.date_modified = date_modified
-        self.risk = risk
-        self.year = year
-        self.month = month
-        self.day = day
 
     def __repr__(self):
         return "<V_FINANCE('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', \
-            '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')>" % (
-            self.date, self.account_id, self.category_id, self.subcategory_id, 
-            self.amount, self.comment, self.stock, self.market, self.shares, 
-            self.price, self.tax, self.commission, self.active, self.date_created,
-            self.date_modified, self.risk, self.year, self.month, self.day)
+            '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', \
+            '%s')>" % (
+            self.finance_id, self.date, self.year, self.month, self.day, self.account_id,
+            self.category_id, self.subcategory_id, self.amount, self.comment,
+            self.stock_name_id, self.shares, self.price, self.tax,
+            self.commission, self.active, self.rate_id,
+            self.currency_exchange_id, self.date_created, self.date_modified)
 
-class V_STOCK(object):
-    """ V_STOCK """
+class V_INVESTMENT(object):
+    """ V_INVESTMENT """
 
-    def __init__(self, finance_id, stock_name_id, action, price, shares, tax, commission, historical, date_created, date_modified, risk):
-        self.finance_id = finance_id
+    def __init__(self, investment_id, stock_name_id, action, price, shares, tax,
+            commission, historical, active, date_created, date_modified, risk):
+        self.investment_id = investment_id
         self.stock_name_id = stock_name_id
         self.action = action
         self.price = price
@@ -62,17 +65,19 @@ class V_STOCK(object):
         self.tax = tax
         self.commission = commission
         self.historical = historical
+        self.active = active
         self.date_created = date_created
         self.date_modified = date_modified
         self.risk = risk
 
     def __repr__(self):
-        return "<V_STOCK('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')>" % (self.finance_id, self.stock_name_id, self.action, self.price, self.shares, self.tax, self.commission, self.historical, self.date_created, self.date_modified, self.risk)
+        return "<V_STOCK('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')>" % (self.investment_id, self.stock_name_id, self.action, self.price, self.shares, self.tax, self.commission, self.historical, self.active, self.date_created, self.date_modified, self.risk)
 
 class V_STOCK_NAME(object):
     """ V_STOCK_NAME """
 
-    def __init__(self, name, market_id, description, date_created, date_modified):
+    def __init__(self, stock_name_id, name, market_id, description, date_created, date_modified):
+        self.stock_name_id = stock_name_id
         self.name = name
         self.market_id = market_id
         self.description = description
@@ -80,33 +85,38 @@ class V_STOCK_NAME(object):
         self.date_modified = date_modified
 
     def __repr__(self):
-        return "<V_STOCK_NAME('%s', '%s', '%s', '%s', '%s')>" % (self.name, self.market_id, self.description, self.date_created, self.date_modified)
+        return "<V_STOCK_NAME('%s', '%s', '%s', '%s', '%s', '%s')>" % (self.stock_name_id, self.name, 
+                self.market_id, self.description, self.date_created, self.date_modified)
 
 class V_MARKET(object):
     """ V_MARKET """
 
-    def __init__(self, code, name, country, date_created, date_modified):
+    def __init__(self, market_id, code, name, country, active, date_created, date_modified):
+        self.market_id = market_id
         self.code = code
         self.name = name
         self.country = country
+        self.active = active
         self.date_created = date_created
         self.date_modified = date_modified
 
     def __repr__(self):
-        return "<V_MARKET('%s', '%s', '%s', '%s', '%s')>" % (self.code, self.name, self.country, self.date_created, self.date_modified)
+        return "<V_MARKET('%s', '%s', '%s', '%s', '%s', '%s', '%s')>" % (self.market_id, self.code, self.name, self.country, self.active, self.date_created, self.date_modified)
 
 class V_CATEGORY(object):
     """ V_CATEGORY """
 
-    def __init__(self, subcategory_id, name, flg_income, date_created, date_modified):
+    def __init__(self, subcategory_id, name, flg_income, active, date_created, date_modified):
         self.subcategory_id = subcategory_id
         self.name = name
         self.flg_income = flg_income 
+        self.active = active
         self.date_created = date_created
         self.date_modified = date_modified
 
     def __repr__(self):
-        return "<V_CATEGORY('%s', '%s', '%s', '%s', '%s')>" % (self.subcategory_id, self.name, self.flg_income, self.date_created, self.date_modified)
+        return "<V_CATEGORY('%s', '%s', '%s', '%s', '%s', '%s')>" % (self.subcategory_id, 
+                self.name, self.flg_income, self.active, self.date_created, self.date_modified)
 
 class V_MARGIN(object):
     """ V_MARGIN """
@@ -136,46 +146,57 @@ class V_MARGIN_TYPE(object):
 class V_SUBCATEGORY(object):
     """ V_SUBCATEGORY """
 
-    def __init__(self, name, date_created, date_modified):
+    def __init__(self, subcategory_id, name, active, date_created, date_modified):
+        self.subcategory_id = subcategory_id
         self.name = name
+        self.active = active
         self.date_created = date_created 
         self.date_modified = date_modified
 
     def __repr__(self):
-        return "<V_SUBCATEGORY('%s', '%s', '%s')>" % (self.name, self.date_created, self.date_modified)
+        return "<V_SUBCATEGORY('%s', '%s', '%s', '%s', '%s')>" % (self.subcategory_id, self.name,
+                self.active, self.date_created, self.date_modified)
 
 class V_ACCOUNT(object):
     """ V_ACCOUNT """
 
-    def __init__(self, name, date_created, date_modified):
+    def __init__(self, account_id, name, description, active, date_created, date_modified):
+        self.account_id = account_id
         self.name = name
+        self.description = description
+        self.active = active
         self.date_created = date_created 
         self.date_modified = date_modified
 
     def __repr__(self):
-        return "<V_ACCOUNT('%s', '%s', '%s')>" % (self.name, self.date_created, self.date_modified)
+        return "<V_ACCOUNT('%s', '%s', '%s', '%s', '%s', '%s')>" % (self.account_id, self.name, 
+                self.description, self.active, self.date_created, self.date_modified)
 
 class V_CURRENCY(object):
     """ V_CURRENCY """
 
-    def __init__(self, code, description):
+    def __init__(self, currency_id, code, description):
+        self.currency_id = currency_id
         self.code = code
         self.description = description
 
     def __repr__(self):
-        return "<V_CURRENCY('%s', '%s')>" % (self.code, self.description)
+        return "<V_CURRENCY('%s', '%s', '%s')>" % (self.currency_id, self.code, self.description)
 
 class V_CURRENCY_EXCHANGE(object):
     """ V_CURRENCY_EXCHANGE """
 
-    def __init__(self, currency_id, exchange_rate, finance_id):
+    def __init__(self, currency_exchange_id, currency_id, exchange_rate,
+            date_created, date_modified):
+        self.currency_exchange_id = currency_exchange_id
         self.currency_id = currency_id 
         self.exchange_rate = exchange_rate
-        self.finance_id = finance_id
+        self.date_created = date_created 
+        self.date_modified = date_modified
 
     def __repr__(self):
-        return "<V_CURRENCY_EXCHANGE('%s', '%s', '%s')>" % (self.currency_id,
-                self.exchange_rate, self.finance_id)
+        return "<V_CURRENCY_EXCHANGE('%s', '%s', '%s', '%s', '%s')>" % (self.currency_exchange_id, self.currency_id,
+                self.exchange_rate, self.date_created, self.date_modified)
 
 class V_FORMULA(object):
     """ V_FORMULA """
@@ -200,7 +221,7 @@ class V_TRADE(object):
             price_buy, price_sell, risk, initial_risk, initial_risk_percent,
             stoploss, profit_loss, profit_loss_percent, r_multiple,
             win_flag, at_work, id_buy, id_sell,
-            currency_id, drawdown_id, date_created, date_modified):
+            currency_id, drawdown_id, active, date_created, date_modified):
         self.trade_id = trade_id 
         self.date_buy = date_buy
         self.year_buy = year_buy
@@ -225,6 +246,7 @@ class V_TRADE(object):
         self.id_buy = id_buy
         self.id_sell = id_sell
         self.currency_id = currency_id
+        self.active = active
         self.date_created = date_created
         self.date_modified = date_modified
 
@@ -232,7 +254,7 @@ class V_TRADE(object):
         return "<V_TRADE('%s', '%s', '%s', '%s', '%s', '%s', \
                 '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', \
                 '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', \
-                '%s', '%s', '%s', '%s')>" % (self.trade_id, 
+                '%s', '%s', '%s', '%s', '%s')>" % (self.trade_id, 
                         self.date_buy,
                         self.year_buy,
                         self.month_buy,
@@ -257,18 +279,21 @@ class V_TRADE(object):
                         self.id_sell,
                         self.currency_id,
                         self.drawdown_id,
+                        self.active,
                         self.date_created,
                         self.date_modified)
 
 class V_RATE(object):
     """ V_RATE """
 
-    def __init__(self, market_id, account_id, extra, extra_percent,
+    def __init__(self, rate_id, market_id, account_id, calculated, calculated_percent,
             on_shares, on_commission, on_ordersize, on_other, commission, tax,
             formula_id, manual_flag, date_created, date_modified):
-        self.market_id = account_id 
-        self.extra = extra
-        self.extra_percent = extra_percent
+        self.rate_id = rate_id
+        self.market_id = market_id
+        self.account_id = account_id
+        self.calculated = calculated
+        self.calculated_percent = calculated_percent
         self.on_shares = on_shares
         self.on_commission = on_commission
         self.on_ordersize = on_ordersize
@@ -282,8 +307,8 @@ class V_RATE(object):
 
     def __repr__(self):
         return "<V_RATE('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', \
-                '%s', '%s', '%s')>" % (self.market_id, self.account_id,
-                        self.extra, self.extra_percent, self.on_shares,
+                '%s', '%s', '%s', '%s', '%s', '%s', '%s')>" % (self.rate_id, self.market_id, self.account_id,
+                        self.calculated, self.calculated_percent, self.on_shares,
                         self.on_commission, self.on_ordersize, self.on_other,
                         self.commission, self.tax, self.formula_id,
                         self.manual_flag, self.date_created, self.date_modified)
@@ -311,5 +336,5 @@ class V_PARAMETER(object):
         self.description = description
 
     def __repr__(self):
-        return "<V_PARAMETER('%s', '%s', '%s', '%s')>" % (self.parameter_id,
-                self.value, self.name, self.description)
+        return "<V_DRAWDOWN('%s', '%s', '%s', '%s')>" % (self.parameter_id, self.name,
+                self.value, self.description)
