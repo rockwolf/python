33b6fa38cb2478587af6ea74e9157bc1849a7cf8
rockwolf <thereisanewway@gmail.com>
2013-02-21 15:54:22 +0100

    date_expiration + expired_flag
    Keep track of expiration dates.
    Will remain the default value of 1900-01-01 for investing, most of the time if not all the time.

--- lisa/setup/sql/create_tables.sql
+++ lisa/setup/sql/create_tables.sql
@@ -172,6 +172,8 @@ CREATE TABLE T_INVESTMENT
     currency_exchange_id int not null default -1,
     drawdown_id int not null default -1,
     pool_investing_at_start decimal(18,6) not null default 0.0,
+    date_expiration datetime not null default '1900-01-01',
+    expired_flag int not null default -1,
     active int not null default 1,
     date_created timestamp not null default '1900-01-01',
     date_modified timestamp not null default '1900-01-01'
@@ -256,6 +258,8 @@ CREATE TABLE T_TRADE
     currency_exchange_id int not null default -1,
     drawdown_id int not null default -1,
     pool_trading_at_start decimal(18,6) not null default 0.0,
+    date_expiration datetime not null default '1900-01-01',
+    expired_flag int not null default -1,
     active int not null default 1,
     date_created timestamp not null default '1900-01-01',
     date_modified timestamp not null default '1900-01-01'
