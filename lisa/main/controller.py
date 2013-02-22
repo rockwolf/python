09685dacd64c15ea4f17044d69082af9db363e69
rockwolf <thereisanewway@gmail.com>
2013-02-22 09:58:10 +0100

    License ref.

--- lisa/main/controller.py
+++ lisa/main/controller.py
@@ -1,27 +1,8 @@
 #!/usr/env/python
 """
-Author: Andy Nagels
-Date: 2010-08-24
-Lisa: Application to store financial transaction information
-      in a PostGresql database.
-
-Copyright (C) 2010 Andy Nagels
-
-This file is part of Lisa.
-
-Lisa is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-Lisa is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-You should have received a copy of the GNU General Public License
-along with Lisa. If not, see <http://www.gnu.org/licenses/>.
-					
+    See LICENSE file for copyright and license details.					
 """
+
 from os.path import isfile
 import shutil
 import os, sys
