bb2f0b681b869e65837057e6649d99054a74de19
rockwolf <thereisanewway@gmail.com>
2013-02-22 15:13:46 +0100

    Kicked to it's own place.

--- lisa/database/databaseaccess.py
+++ lisa/database/databaseaccess.py
@@ -353,35 +353,6 @@ class DatabaseAccess():
             result = (price_buy > price_sell)
         return 1 if result else 0
 
-    def create_statements_TABLE_CURRENCY_EXCHANGE(self, input_fields):
-        """
-            Creates the records needed for TABLE_CURRENCY_EXCHANGE.
-        """
-        try:
-            statement_currency_exchange = Statement(TABLE_CURRENCY_EXCHANGE)
-            date_created = current_date()
-            date_modified = current_date()
-            records = 0
-            for fields in input_fields:
-                records = records + 1
-                #NOTE: we don't need to query, because we always add a new
-                #currency_exchange line. The same value can be used multiple
-                #times, so it's not possible to query if one already exists.
-                statement_currency_exchange.add(
-                    records,
-                    {
-                        'currency_exchange_id':None,
-                        'from_currency_id':self.currency_id_from_currency(fields['from_currency']),
-                        'to_currency_id':int(self.config.default_currency),
-                        'exchange_rate':Decimal(fields['exchange_rate']),
-                        'date_created':date_created,
-                        'date_modified':date_modified
-                    }
-                )
-            return statement_currency_exchange
-        except Exception as ex:
-            print(ERROR_CREATE_STATEMENTS_TABLE_CURRENCY_EXCHANGE, ex)
-
     def calculate_commission(self):
         """
             Calculation for T_RATE.
