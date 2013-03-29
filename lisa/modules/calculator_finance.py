5de941f9fdaadd4cb11243de1e26ee6f3a05be07
Andy Nagels <thereisanewway@gmail.com>
2013-03-30 00:09:42 +0100

    Calling the haskell library.

--- lisa/modules/calculator_finance.py
+++ lisa/modules/calculator_finance.py
@@ -15,14 +15,14 @@ def calculate_stoploss(self, amount_buy_simple, shares_buy, tax_buy, commission_
         Calculates the stoploss.
     """
     #TODO: use the below for all calls
-    return lib.CalcStoploss(...)
+    return lib.CalcStoploss(amount_buy_simple, shares_buy, tax_buy, commission_buy, i_risk, pool_at_start)
 
 def calculate_risk_input(self, i_pool, i_risk):
     """
         Calculates the risk based on total pool and input.
         Consider this the theoretical risk we want to take.
     """
-    return 
+    return lib.CalcRiskInput(i_pool, i_risk)
 
 def calculate_risk_initial(self, price_buy, shares_buy, stoploss):
     """
@@ -31,54 +31,53 @@ def calculate_risk_initial(self, price_buy, shares_buy, stoploss):
         This should be equal to the risk_input if everything was
         correctly calculated.
     """
-    return 
+    return lib.CalcRiskInitial(price_buy, shares_buy, stoploss)
 
 def calculate_risk_actual(self, price_buy, shares_buy, price_sell, shares_sell, stoploss, risk_initial):
     """
         Calculates the risk we actually took,
         based on the data in TABLE_TRADE.
     """
-    return 
+    return lib.CalcRiskActual(price_buy, shares_buy, price_sell, shares_sell, stoploss, risk_initial)
 
 def calculate_r_multiple(self, price_buy, price_sell, stoploss):
     """ 
         Function to calculate R-multiple.
     """
-    return 
+    return lib.CalcRMultiple(price_buy, price_sell, stoploss)
 
 def calculate_cost_total(self, tax_buy, commission_buy, tax_sell, commission_sell):
     """
         Returns the total costs associated with the given trade.
     """
-    return 
+    return lib.CalcCostTotal(tax_buy, commission_buy, tax_sell, commission_sell)
 
 def calculate_amount_simple(self, price, shares):
     """
         Calculates the amount without tax and commission.
     """
-    return
+    return lib.CalcAmountSimple(price, shares)
 
 def cost_transaction(self, transaction, price, shares, tax, commission):
     """
         Cost of transaction (tax and commission)
     """
-    return
+    return lib.CostTransaction(transaction, price, shares, tax, commission)
 
 def calculate_profit_loss(self, amount_sell_simple, amount_buy_simple, total_cost):
     """
         Calculates the profit_loss.
     """
-    return 
+    return lib.CalcProfitLoss(amount_sell_simple, amount_buy_simple, total_cost)
 
 def calculate_cost_other(self, total_cost, profit_loss):
     """
         Calculates others costs based on the difference that remains.
     """
-    return
+    return lib.CalcCostOther(total_cost, profit_loss)
 
-def calculate_commission(self):
+def calculate_commission(self, account, market, stockname, price, shares):
     """
         Calculation for T_RATE.
     """
-    #TODO: Put all the commission stuff in the CalculatorFinance.hs lib and link to it here.
-    return 
+    return lib.CalcCommission(account, market, stockname, price, shares)
