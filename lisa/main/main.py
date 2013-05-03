3cac1ff33c3385e7e7417165f5ca7268ec8d18bb
rockwolf <thereisanewway@gmail.com>
2013-05-03 12:35:01 +0300

    Cleanup.

--- lisa/main/main.py
+++ lisa/main/main.py
@@ -1,26 +1,6 @@
 #!/usr/env/python
 """
-Author: Andy Nagels
-Date: 2010-08-24
-Lisa: Less Interaction Saves Action
-A frontend for a database to store financial transactions in a convenient way.
-
-Copyright (C) 2010 Andy Nagels
-
-This file is part of Lisa.
-
-Lisa is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-Lisa is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-You should have received a copy of the GNU General Public License
-along with Lisa. If not, see <http://www.gnu.org/licenses/>.
-					
+See LICENSE file for copyright and license details.
 """
 import sys
 
@@ -31,15 +11,14 @@ from modules.fileexport import FileExport
 from setup.setup import Setup
         
 class MainWrapper():
-    """ Main logic 
-    
-    Parsed options get there functionality here,
-    it's seperate from the gui part of the app.
-    
+    """
+        Set system paths and run the app. 
     """ 
     
     def __init__(self):
-        """ Set program params and python path and load the config. """
+        """ 
+            Set program params and python path and load the config.
+        """
         self.exitstate = 0   
 
         # Adjust system path so we can import from our
@@ -53,7 +32,7 @@ class MainWrapper():
 
     def adjust_system_path(self):
         """
-        Adjust the system path, so we can search in custom dirs for modules.
+            Adjust the system path, so we can search in custom dirs for modules.
         """
         sys.path.append('main')
         sys.path.append('pyqt')
@@ -65,7 +44,9 @@ class MainWrapper():
         sys.path.append('setup')
     
     def file_import(self):
-        """ import """
+        """
+            import
+        """
         setup = Setup()
         setup.clear_tables()
         setup.drop_constraints()
@@ -76,14 +57,17 @@ class MainWrapper():
         imp = None
 
     def file_export(self):
-        """ export """
+        """
+            export
+        """
         exp = FileExport(self.config)
         exp.file_export()
         exp = None
 
-
     def run(self):
-        """ This is the main driver for the program. """
+        """
+            This is the main driver for the program.
+        """
         #TODO: put the QTGui crap in the view and call that from the
         #controller. This part of the code should be used to start the
         #controller.
