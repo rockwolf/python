24d6359a7bcc89444c6f6e25afb763c2bcb7cae3
rockwolf <thereisanewway@gmail.com>
2013-05-24 11:09:34 +0200

    Option handling.

--- emma/__init__.py
+++ emma/__init__.py
@@ -8,7 +8,7 @@ import argparse
 
 from setup.setup import Setup
 
-def main(option, export_type):
+def main(use, pool, tax, commission, shares, price, automatic):
     """ Main driver. """
     ### Run the application ###
     #NOTE: the import statement loads the views and tables,
@@ -25,6 +25,19 @@ def main(option, export_type):
     #TODO: add stuff for export to ledger format?
     wrapper.run() #run the main method for the program
       
+      
+def install():
+    """ install """
+    setup = Setup()
+    setup.install()
+    setup = None
+
+def uninstall():
+    """ uninstall """
+    setup = Setup()
+    setup.uninstall()
+    setup = None
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Equations for Money Management Application")
     parser.add_argument(
@@ -43,18 +56,37 @@ if __name__ == "__main__":
     parser.add_argument(
     	'-c',
         '--commission',
-        help='For a setup, though .',
+        help='Commission',
         default=False,
-        action='store_true')
+        action='store')
     parser.add_argument(
-        '--uninstall',
-        help='Removes all records and tables from the database!',
+        '-t',
+        '--tax',
+        help='Tax amount to use: <double>',
+        default=0.0025,
+        action='store')
+    parser.add_argument(
+        '-s,
+        '--shares',
+        help='Tax amount to use: <double>',
+        default=1,
+        action='store')
+    parser.add_argument(
+        '-p,
+        '--price
+        help='Price: <double>',
+        default=0.0
+        action='store')
+    parser.add_argument(
+        '-a',
+        '--automatic',
+        help='Override tax/commission values with the values from the library!',
         default=False,
         action='store_true')
     parser.add_argument(
         '-V',
         '--version',
-        help='Shows application version.',
+        help='Shows application version and usage.',
         default=False,
         version='Emma 1.00',
         action='version')
@@ -65,24 +97,15 @@ if __name__ == "__main__":
         action='store_true')
     args = vars(parser.parse_args())
    
-    option = ''
-    export_type = ''
-    if args['import']:
-        option = 'import'
-    elif args['export']:
-        option = 'export'
-        if args['export'] == EXPORT_LEDGER:
-            export_type = EXPORT_LEDGER
-        elif args['export'] == EXPORT_CSV:
-            export_type = EXPORT_CSV
-        else:
-            print("Error: wrong export type (" +
-                    args['export'] +
-                    "), falling back on the default (" +
-                    EXPORT_CSV +
-                    ")!");
-            export_type = EXPORT_CSV
-    elif args['install']:
+    #TODO: add value checking here, so we don't enter wrong types etc.
+    use = args['use']
+    pool = args['pool']
+    tax = args['tax']
+    commission = args['commission']
+    shares = args['shares']
+    price = args['price']
+    
+    if args['install']:
         install()
         sys.exit(0)
     elif args['uninstall']:
@@ -91,4 +114,4 @@ if __name__ == "__main__":
     elif args['python']:
         print('Python ' + sys.version)
         sys.exit(0)
-    main(option, export_type)
+    main(use, pool, tax, commission, shares, price)
