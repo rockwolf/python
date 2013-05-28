266609e8408b493602e8121c904668a0b4a550bb
rockwolf <thereisanewway@gmail.com>
2013-05-28 14:59:43 +0300

    extending the code

--- emma/modules/calculator.py
+++ emma/modules/calculator.py
@@ -10,7 +10,7 @@ class Calculator():
         Calculator class
     """
    
-    def __init__(self, pool, amount, tax, commission, shares, price, buy, market, commodity):
+    def __init__(self, pool, amount, tax, commission, shares, price, buy, market, commodity, self.account):
         """
             Initialize
         """
@@ -22,17 +22,35 @@ class Calculator():
         self.price = price
         self.buy = buy
         self.market = market
+        self.account = account
   
     def calculate(self):
         """
             Calculate all possible unknown values.
         """
         try:
-           if self.price == Decimal(-1.0):
-               #price not known, calculate it
-               self.price = calculate_price(self.amount, self.shares, self.tax, self.commission)
-               print('price =', self.price)
-           #TODO: determine what fields are given and calculate accordingly
+            #Note: the order is important...
+            # Input values
+            if self.shares == Decimal(-1.0):
+                self.shares = calculate_shares_recommended()
+            if self.price == Decimal(-1.0):
+                self.price = calculate_price(
+                    self.amount
+                    , self.shares
+                    , self.tax
+                    , self.commission)
+                print('price =', self.price)
+            if self.commission == Decimal(-1.0):
+                self.commission = calculate_commission(
+                    self.account
+                    , self.market
+                    , self.commodity
+                    , self.price
+                    , self.shares)
+            if self.amount == decimal(-1.0):
+                self.amount = calculate_amount(...) #TODO: finish this, but I'm first going to add it to the library.
+            # Extra calculatable fields
+            #TODO: calculate stoploss etc.
         except Exception as ex:
             print('Error in calculate:', ex)
 
