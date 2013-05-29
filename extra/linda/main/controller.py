29be8b657eed4b68e7c69bfd9c601c406fd3fe42
rockwolf <thereisanewway@gmail.com>
2013-05-29 15:35:34 +0200

    More logic for getting the drawdown value.

--- extra/linda/main/controller.py
+++ extra/linda/main/controller.py
@@ -14,14 +14,27 @@ from modules.constant import *
 class ControllerMain():
     """ Contains the bussiness logic of the application. """
     
-    def __init__(self, config):
+    def __init__(self, config, days):
         """ Initialize """
-        # initialise special vars
+        # initialise vars
         self.config = config
-        
+        self.days = days
 
     # Methods
     ## General
-    def run(self):
+    def run(self, update):
         """ Run the app. """
-        print('Not implemented yet.')
+        #TODO: show list of drawdown values and trades for <limit> latest records
+        #ask for an id
+        # when not update: ask for a number of days
+        #TODO: open dba session to call for the codes
+        drawdown_id = dba.get_latest_drawdown_id()
+        input_drawdown_id = input('Enter drawdown_id [' + str(drawdown_id) + ': ')
+        if input_drawdown_id != '':
+           drawdown_id = int(input_drawdown_id)
+        if update:
+           day = dba.get_drawdown_value(drawdown_id)
+        else:
+            day = input('Enter day of drawdown: ')
+        print('day =', day)
+        update_drawdown_value(drawdown_id, day)
