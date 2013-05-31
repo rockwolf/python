99cf75030e647d8e731a8a7ac55e22032da4985c
Andy Nagels <thereisanewway@gmail.com>
2013-05-31 23:39:12 +0200

    Corrections, the installer works again.

--- lisa/setup/sql/create_tables.sql
+++ lisa/setup/sql/create_tables.sql
@@ -27,7 +27,6 @@ CREATE TABLE T_POOL
 CREATE TABLE T_MARKET
 (
     market_id int not null,
-    account_id int not null,
     code varchar(50) not null,
     name varchar(30) not null,
     country char(3) not null,
