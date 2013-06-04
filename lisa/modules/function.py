8246a177e25ac602a622035df4046e6317d7d4ab
rockwolf <thereisanewway@gmail.com>
2013-06-04 11:34:18 +0200

    building the account tree

--- lisa/modules/function.py
+++ lisa/modules/function.py
@@ -35,3 +35,40 @@ def we_are_buying(account_name):
     """ Are we buying? """
     #TODO: fix this function with the new way of working
     return subcategory == 'buy'
+
+def build_account_tree(account_list):
+    """
+        Returns a list of all accounts:
+        Example: input = ['test1', 'test2:test2a:test2a1', 'test3:test3c']
+        output: result = ['test1'
+            , 'test2'
+            , 'test2:test2a'
+            , 'test2:test2a:test2a1'
+            , 'test3'
+            , 'test3:test3c']
+    """
+    result = []
+    for account in account_list:
+        result.append(delimiter_tree_value(account, ':'))
+    return result
+    
+def delimiter_tree_values(value_string, delimiter):
+    """
+        Turns a <delimiter>-separated string into
+        subelements in a tree.
+        Example: input = "this:is:an:example"
+        with delimiter = ':'
+        output: result = ['this'
+            , 'this:is'
+            , 'this:is:an'
+            , 'this:is:an:example']
+    """
+    #TODO: remove last : ?
+    result = []
+    for index, part in enumerate(longest_account.split(':')):
+        if index == 0:
+            next = part
+        else:
+            next = next + ':' + part
+        result.append(next)
+    return result
