0eb0f73d892997148cb591ebeb807ff99a9500e1
rockwolf <thereisanewway@gmail.com>
2013-06-10 16:45:03 +0300

    Cleanup.

--- lisa/modules/function.py
+++ lisa/modules/function.py
@@ -21,17 +21,23 @@ def is_an_investment(account_name):
     return 'investing' in account_name.lower()
 
 def deals_with_stocks(account_name):
-    """ See if we need to use rate, marketid and stockid. """
+    """
+        See if we need to use rate, marketid and stockid.
+    """
     #TODO: fix this function with the new way of working
     return (is_a_trade(account_name) or
             is_an_investment(account_name))
 
 def is_a_table(key):
-    """ Used to ignore all dictionary entries that don't start with t_ """
+    """
+        Used to ignore all dictionary entries that don't start with t_
+    """
     return key.lower()[0:2] == 't_'
 
 def we_are_buying(account_name):
-    """ Are we buying? """
+    """
+        Are we buying?
+    """
     #TODO: fix this function with the new way of working
     return subcategory == 'buy'
 
