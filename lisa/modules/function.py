070e52a010812df6bd54eab9143276da9e797180
rockwolf <thereisanewway@gmail.com>
2013-06-12 11:08:08 +0300

    Added is_negative_amount

--- lisa/modules/function.py
+++ lisa/modules/function.py
@@ -36,20 +36,35 @@ def is_an_investment(account_to):
             result = False
             break
     return result
+    
+def is_negative_amount(account_name):
+    """
+        Check if the amount we enter should be positive or negative.
+        Rules:
+        Debit all expenses and losses, credit all incomes and gains
+        Debit all assets, credit all liabilities
+        Debit the receiver, credit the giver
+    """
+    result = False
+    for name in NEGATIVES:
+        if name.lower() in account_name.lower():
+            result = True
+            break;
+    return result
 
-def deals_with_stocks(account_name):
+def deals_with_stocks(account_to):
     """
         See if we need to use rate, marketid and stockid.
     """
     #TODO: fix this function with the new way of working
-    return (is_a_trade(account_name) or
-            is_an_investment(account_name))
+    return (is_a_trade(account_to) or
+            is_an_investment(account_to))
 
 def is_a_table(key):
     """
         Used to ignore all dictionary entries that don't start with t_
     """
-    return key.lower()[0:2] == 't_'
+    return key.lower().startswith('t_')
 
 def we_are_buying(account_to):
     """
