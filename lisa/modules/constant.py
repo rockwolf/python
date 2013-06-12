77426f524eb13f5402badc80224bd57414ab6559
rockwolf <thereisanewway@gmail.com>
2013-06-12 12:42:42 +0300

    Simplifying the account structure.

--- lisa/modules/constant.py
+++ lisa/modules/constant.py
@@ -54,7 +54,6 @@ class Error():
     GET_MARKET_DESCRIPTION = "Error in get_marketdescription: "
     GET_CATEGORIES = "Error in get_categories: "
     GET_ACCOUNTS = "Error in get_accounts: "
-    GET_FULL_ACCOUNTS = "Error in get_full_accounts: "
     GET_MARKETS = "Error in get_markets: "
     GET_STOCK_NAMES = "Error in get_stocknames: "
     GET_STOCK_DESCRIPTION = "Error in get_stockdescription: "
