1782146f9640282e931f1f05aaea2fce58a96485
rockwolf <thereisanewway@gmail.com>
2013-09-16 14:19:40 +0200

    Reference import to generic modules.
    Is that the correct way of doing this?

--- lisa/lisa/modules/investment.py
+++ lisa/lisa/modules/investment.py
@@ -9,9 +9,9 @@ from modules.core_module import CoreModule
 from modules.statement import Statement
 from modules.constant import *
 from modules.function import *
-from modules_generic.function import *
+from generic.modules.function import *
 from database.mappings import T_INVESTMENT
-from modules_generic.calculator_finance import *
+from generic.modules.calculator_finance import *
 
 class Investment(CoreModule):
     """
