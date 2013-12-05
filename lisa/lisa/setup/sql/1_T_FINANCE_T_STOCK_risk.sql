1811ad710fe6fcc4c817befa42543c905369dc51
rockwolf <thereisanewway@gmail.com>
2013-12-05 14:06:34 +0100

    Cleanup.

--- lisa/lisa/setup/sql/1_T_FINANCE_T_STOCK_risk.sql
+++
@@ -1,6 +0,0 @@
-/* Author: rockwolf
- * Date: 2012-03-18
- * Function: add risk column to T_FINANCE and T_STOCK.
- */
-alter table t_finance add risk numeric(18, 4) not null default 0.0;
-alter table t_stock add risk numeric(18, 4) not null default 0.0;
