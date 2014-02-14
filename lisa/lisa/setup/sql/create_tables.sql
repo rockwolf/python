e0ae63c4482dd2cce1d075adb5457049b28695b7
rockwolf <thereisanewway@gmail.com>
2014-02-14 09:43:08 +0100

    Added spread to T_COMMODITY

--- lisa/lisa/setup/sql/create_tables.sql
+++ lisa/lisa/setup/sql/create_tables.sql
@@ -51,6 +51,7 @@ CREATE TABLE T_COMMODITY
     order_max decimal(18, 6) not null default -1.0,
     margin_day_proc decimal(18, 6) not null default -1.0,
     margin_night_proc decimal(18, 6) not null default -1.0,
+    spread decimal(18,6) not null default 0.0,
     date_created timestamp not null default '1900-01-01',
     date_modified timestamp not null default '1900-01-01',
     unique (name, market_id)
