6285f6b105104a47e4c8f378fe9d2eafd5c34817
rockwolf <thereisanewway@gmail.com>
2014-03-12 16:18:27 +0100

    Default commodity_general record.

--- lisa/lisa/setup/sql/init_tables.sql
+++ lisa/lisa/setup/sql/init_tables.sql
@@ -81,8 +81,12 @@ INSERT INTO T_CURRENCY(currency_id, code, description)
 values(14, 'RUB', 'Russian Ruble');
 
 /* commodity names */
-INSERT INTO T_COMMODITY(name, market_id, description, active, date_created, date_modified)
+INSERT INTO T_COMMODITY(name, market_id, description, commodity_general_id, active, date_created, date_modified)
 values('', 1, '', 1, current_date, current_date);
+
+/* commodity general */
+INSERT INTO T_COMMODITY_GENERAL(name, market_id, currency_id, tick, tick_value, order_min, order_max, margin_day_proc, margin_night_proc, spread, date_created, date_modified)
+values('', 1, 1, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, current_date, current_date);
 
 /* t_rate */
 INSERT INTO T_RATE(commission, tax, automatic_flag, date_created, date_modified)
