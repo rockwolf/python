42a308dc1f49c48bc9a73c4adf6ad8611fefce49
rockwolf <thereisanewway@gmail.com>
2014-04-17 15:34:47 +0200

    Create plot_networth.py
    skeleton

---
+++ ledgerplot/plot_networth.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+
+"""
+    See LICENSE.txt file for copyright and license details.
+"""
+
+
+"""
+    A plot with (the evolution of) the networth.
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+from decimal import Decimal
+import sys
