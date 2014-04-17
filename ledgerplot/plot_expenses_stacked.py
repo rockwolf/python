ec2ac884ba73eaf2e633c3437cad322a72f9700d
rockwolf <thereisanewway@gmail.com>
2014-04-17 15:33:27 +0200

    Create plot_expenses_stacked.py
    skeleton

---
+++ ledgerplot/plot_expenses_stacked.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+"""
+    See LICENSE.txt file for copyright and license details.
+"""
+
+
+"""
+    A bar plot with income and expense levels.
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+from decimal import Decimal
+import sys
