2e0e2f205a68a681bd2e8fb4812cb0fc69a0bc74
rockwolf <thereisanewway@gmail.com>
2014-04-17 15:36:41 +0200

    Create plot_crossover.py
    skeleton

---
+++ ledgerplot/plot_crossover.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+"""
+    See LICENSE.txt file for copyright and license details.
+"""
+
+
+"""
+    A plot that shows when break even is reached.
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+from decimal import Decimal
+import sys
