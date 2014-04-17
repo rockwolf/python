e2b539935b8c2aa0d965cbc02069228034c6a6bb
rockwolf <thereisanewway@gmail.com>
2014-04-17 15:32:38 +0200

    Create plot_expenses.py
    Created skeleton

---
+++ ledgerplot/plot_expenses.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+
+"""
+    See LICENSE.txt file for copyright and license details.
+"""
+
+
+"""
+    A bar plot with expense levels per category.
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+from decimal import Decimal
+import sys
