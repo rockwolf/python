76a334e10886f13aeadf743ec239dae321c0ee86
rockwolf <thereisanewway@gmail.com>
2014-06-30 22:55:28 +0200

    rellotge + files that still weren't committed.

--- ledgerplot/plot_crossover.py
+++ ledgerplot/plot_crossover.py
@@ -11,3 +11,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 from decimal import Decimal
 import sys
+
+x_array = []
+y_array = []
+
+def load_data():
+    """
+        Load data
+    """
+    var_data = open(sys.argv[1].strip(), 'r').read()
+    var_data_array = var_data.split('\n')
+    i = 0
+    for line in var_data_array:
+        i += 1
+        # skip the last 2 lines of the output
+        if (len(line)>1) and (i<len(var_data_array) - 2):
+            x_array.append(abs(float(line.strip().split(' ')[0].strip())))
+            y_array.append(i)
+
---
+++ lisa/lisa/setup/sql/create_views_extra.sql
@@ -0,0 +1,26 @@
+BEGIN;
+
+/* V_COMMODITY_INFO */
+--DROP VIEW V_COMMODITY_INFO;
+CREATE VIEW V_COMMODITY_INFO
+AS
+select
+    c.commodity_id
+    , c.name as commodity_name
+    , c.description as commodity_description
+    , ct.name as commodity_type
+    , ct.description as commodity_type_description
+    , cfd.name as cfd_name
+    , m.market_id
+    , m.name as market
+    , m.code as market_code
+    , m.country as market_country
+from
+    t_commodity c
+    inner join t_commodity_type ct on c.commodity_type_id = ct.commodity_type_id
+    left join t_cfd_general cfd on c.cfd_general_id = cfd.cfd_general_id
+    inner join t_market m on cfd.market_id = m.market_id
+where c.active = 1
+;
+
+COMMIT;
