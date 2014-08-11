eda42320d73814d2a660499a5f4940d2216a3c22
rockwolf <thereisanewway@gmail.com>
2014-08-11 16:40:30 +0200

    constant.py

---
+++ ledgerplot/constant.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+"""
+See LICENSE.txt file for copyright and license details.
+"""
+
+class PlotIncomeVsExpensesType():
+    ALL_DATA_UNTIL_NOW = 0
+    ALL_DATA_FOR_GIVEN_YEAR = 1
+    ALL_DATA_FOR_GIVEN_PERIOD = 2
+    ALL_DATA_FOR_GIVEN_PERIOD_TOTAL = 3
