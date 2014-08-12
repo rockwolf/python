d0b7986110ba7e5c60e9df518067f9ad080dfc02
rockwolf <thereisanewway@gmail.com>
2014-08-12 08:52:07 +0200

    DatFile enum

--- ledgerplot/constant.py
+++ ledgerplot/constant.py
@@ -8,3 +8,6 @@ class PlotIncomeVsExpensesType():
     ALL_DATA_FOR_GIVEN_YEAR = 1
     ALL_DATA_FOR_GIVEN_PERIOD = 2
     ALL_DATA_FOR_GIVEN_PERIOD_TOTAL = 3
+
+class DatFile():
+    INCOME_VS_EXPENSES = 'income_vs_expenses.dat'
