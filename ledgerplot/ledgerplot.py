a8b5e3492ccc2d256fa2eb4f3c1ad606b0281711
rockwolf <thereisanewway@gmail.com>
2014-08-12 08:58:24 +0200

    Some improvements.

--- ledgerplot/ledgerplot.py
+++ ledgerplot/ledgerplot.py
@@ -5,7 +5,8 @@
     
     Options:
         --ledger <ledger file>
-        --income_vs_expenses [<year>|<start date> <end date> [total]]
+        --income_vs_expenses [<year>|<start date> <end date>]
+        --total
         -V, --version
 """
 
@@ -22,6 +23,30 @@ from constant.py import *
 __all__ = ['ledgerplot']
 __version__ = 'v0.1'
     
+    
+def plot_income_vs_expenses(is_total):
+    """
+        Plot income  vs expenses.
+    """
+    from plot_income_vs_expenses import PlotIncomeVsExpenses
+    plot = PlotIncomeVsExpenses()
+    arglen = len(args['--income_vs_expenses'])
+    print '-TEST- len(args[--income_vs_expenses]=%d'.format(arglen)
+    if isTotal and arglen = 2:
+        plot.prepare_data(PlotIncomeVsExpensesType.ALL_DATA_FOR_GIVEN_PERIOD_TOTAL)
+    elif not isTotal and arglen = 2:
+        plot.prepare_data(PlotIncomeVsExpensesType.ALL_DATA_FOR_GIVEN_PERIOD)
+    elif arglen = 1:
+        plot.prepare_data(PlotIncomeVsExpensesType.ALL_DATA_FOR_GIVEN_YEAR)
+    elif arglen = 0:
+        plot.prepare_data(PlotIncomeVsExpensesType.ALL_DATA_UNTIL_NOW)
+    else:
+        print 'Too many arguments.'
+    plot.dat_file = DatFile.INCOME_VS_EXPENSES
+    plot.load_data()
+    plot.plot_data(year, start_date, end_date, total)
+    plot = None
+        
 if __name__ == "__main__":
     args = docopt(__doc__, help=True, version=__version__)
     
@@ -29,21 +54,8 @@ if __name__ == "__main__":
         ledger_file = args['--ledger']
         print 'Using ledger file %s'.format(ledger_file)
         
+    is_total = args['--total']
+        
     if args['--income_vs_expenses']:
-        from plot_income_vs_expenses import PlotIncomeVsExpenses
-        plot = PlotIncomeVsExpenses()
-        arglen = len(args['--income_vs_expenses'])
-        print '-TEST- len(args[--income_vs_expenses]=%d'.format(arglen)
-        if arglen = 3:
-            plot.prepare_data(PlotIncomeVsExpensesType.ALL_DATA_FOR_GIVEN_PERIOD_TOTAL)
-        elif arglen = 2:
-            plot.prepare_data(PlotIncomeVsExpensesType.ALL_DATA_FOR_GIVEN_PERIOD)
-        elif arglen = 1:
-            plot.prepare_data(PlotIncomeVsExpensesType.ALL_DATA_FOR_GIVEN_YEAR)
-        elif arglen = 0:
-            plot.prepare_data(PlotIncomeVsExpensesType.ALL_DATA_UNTIL_NOW)
-        else:
-            print 'Too many arguments.'
-            
-        plot = None
+        plot_income_vs_expenses(is_total)
     sys.exit(0)
