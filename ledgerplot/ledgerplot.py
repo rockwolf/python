4000ecb87e3f08760042cf638f6f4401e24e33bf
rockwolf <thereisanewway@gmail.com>
2014-08-14 15:37:35 +0200

    prepare_data parameters

--- ledgerplot/ledgerplot.py
+++ ledgerplot/ledgerplot.py
@@ -24,22 +24,22 @@ __all__ = ['ledgerplot']
 __version__ = 'v0.1'
     
     
-def plot_income_vs_expenses(is_total):
+def plot_income_vs_expenses(year, start_date, end_date, is_total, arglen):
     """
         Plot income  vs expenses.
     """
     from plot_income_vs_expenses import PlotIncomeVsExpenses
     plot = PlotIncomeVsExpenses()
-    arglen = len(args['--income_vs_expenses'])
+    arglen = len(arg_ive)
     print '-TEST- len(args[--income_vs_expenses]=%d'.format(arglen)
-    if is_total and (arglen == 2):
-        plot.prepare_data(PlotIncomeVsExpensesType.ALL_DATA_FOR_GIVEN_PERIOD_TOTAL)
-    elif not is_total and (arglen == 2):
-        plot.prepare_data(PlotIncomeVsExpensesType.ALL_DATA_FOR_GIVEN_PERIOD)
+    if is_total and (arglen == 3):
+        plot.prepare_data(PlotIncomeVsExpensesType.ALL_DATA_FOR_GIVEN_PERIOD_TOTAL, year, start_date, end_date, is_total)
+    elif not is_total and (arglen == 3):
+        plot.prepare_data(PlotIncomeVsExpensesType.ALL_DATA_FOR_GIVEN_PERIOD, year, start_date, end_date, is_total)
+    elif arglen == 2:
+        plot.prepare_data(PlotIncomeVsExpensesType.ALL_DATA_FOR_GIVEN_YEAR, year, start_date, end_date, is_total)
     elif arglen == 1:
-        plot.prepare_data(PlotIncomeVsExpensesType.ALL_DATA_FOR_GIVEN_YEAR)
-    elif arglen == 0:
-        plot.prepare_data(PlotIncomeVsExpensesType.ALL_DATA_UNTIL_NOW)
+        plot.prepare_data(PlotIncomeVsExpensesType.ALL_DATA_UNTIL_NOW, year, start_date, end_date, is_total)
     else:
         print 'Too many arguments.'
     plot.dat_file = DatFile.INCOME_VS_EXPENSES
@@ -56,7 +56,13 @@ if __name__ == "__main__":
         
     is_total = args['--total']
     print 'Total = {}'.format(is_total)
-        
+    
+    arg_ive = args['--income_vs_expenses']
+    arglen = len(arg_ive)
+    year = arg_ive[0]
+    start_date = arg_ive[1]
+    end_date = arg_ive[2]
+
     if args['--income_vs_expenses']:
-        plot_income_vs_expenses(is_total)
+        plot_income_vs_expenses(year, start_date, end_date, is_total, arglen)
     sys.exit(0)
