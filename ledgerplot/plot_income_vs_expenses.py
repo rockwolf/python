7c7614eeb663eeff9060a5796a9baa0667bd8c4d
rockwolf <thereisanewway@gmail.com>
2014-08-14 15:16:36 +0200

    More updates.

--- ledgerplot/plot_income_vs_expenses.py
+++ ledgerplot/plot_income_vs_expenses.py
@@ -24,15 +24,18 @@ class PlotIncomeVsExpenses():
         """
         self.x_array = []
         self.y_array = []
+        self.profit = []
         self.dat_file = ''
 
     def prepare_data(pive_type = PlotIncomeVsExpensesType.ALL_DATA_UNTIL_NOW):
         """
             Extract the data we want to plot from ledger.
         """
+        #TODO: change $2 etc. to the correct input value from the cli parameters
+        #TODO: implement ledger_file var?
         try:
             if pive_type == PlotIncomeVsExpensesType.ALL_DATA_FOR_GIVEN_PERIOD_TOTAL:
-                call(['sh', 'ledger -f $1 --real -s -d "T&l<=1" -b $2 -e $3 bal -Equity -^assets expenses income > income_vs_expenses.dat'])
+                call(['sh', 'ledger -f {} --real -s -d "T&l<=1" -b $2 -e $3 bal -Equity -^assets expenses income > income_vs_expenses.dat'.format(ledger_file)])
             elif pive_type == PlotIncomeVsExpensesType.ALL_DATA_FOR_GIVEN_PERIOD:
                 call(['sh', 'ledger -f $1 --real -s -d "T&l<=1" --begin $2 --end $3 bal --period-sort --monthly -Equity -^assets expenses income > income_vs_expenses.dat'])
             elif pive_type == PlotIncomeVsExpensesType.ALL_DATA_FOR_GIVEN_YEAR:
@@ -54,7 +57,8 @@ class PlotIncomeVsExpenses():
             # skip the last 2 lines of the output
             if (len(line)>1) and (i<len(var_data_array) - 2):
                 self.x_array.append(abs(float(line.strip().split(' ')[0].strip())))
-                y_array.append(i)
+                self.y_array.append(i)
+                self.profit.append(
                 
     def autolabel(self, rects):
         """
@@ -71,15 +75,15 @@ class PlotIncomeVsExpenses():
             Plots the loaded data.
         """
         N = 1
-        income = self.x_array[1] # ledger shows expenses first,
-                                 # so reverse to get income vs expenses.
-        expenses = self.x_array[0]
+        income = self.x_array[0]
+        expenses = self.x_array[1]
 
         ind = np.arange(N)  # the x locations for the groups
         width = 0.15       # the width of the bars
         fig, ax = plt.subplots()
-        rects1 = ax.bar(ind+width, expenses, width, color='r')
-        rects2 = ax.bar(ind, income, width, color='g')
+        rects1 = ax.bar(ind, expenses, width, color='r')
+        rects2 = ax.bar(ind+width, income, width, color='g')
+        rects3 = ax.bar(ind+2*width, (expenses - income), width, color='b')
         # add some labels/text
         ax.set_ylabel('Value (EUR)')
         title_year = ''
@@ -88,9 +92,10 @@ class PlotIncomeVsExpenses():
         ax.set_title('Income vs. expenses{}'.format(title_year))
         ax.set_xticks(ind+width)
         #ax.set_xticklabels( ('2014') ) #TODO: use the year from the input? But do we need this?
-        ax.legend((rects1[0], rects2[0]), ('Expenses', 'Income'))
+        ax.legend((rects1[0], rects2[0], rects3[0]), ('Expenses', 'Income', 'Profit'))
         autolabel(rects1)
-        autolabel(rects2)=
+        autolabel(rects2)
+        autolabel(rects3)
         plt.show()
 
 if __name__ == "__main__":
