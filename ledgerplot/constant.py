c14a703bc4a641f6cdc9fd94f2ffcdf98379aa9a
rockwolf <thereisanewway@gmail.com>
2015-03-10 15:59:35 +0100

    Added ErrorMsg class and
    renamed the first enum.

--- ledgerplot/constant.py
+++ ledgerplot/constant.py
@@ -3,14 +3,30 @@
 See LICENSE.txt file for copyright and license details.
 """
 
-class PlotIncomeVsExpensesType():
+class PlotDataRetrievalType():
+    """
+        Types that describe different ways to
+        get data for a plot.
+    """
     ALL_DATA_UNTIL_NOW = 0
     ALL_DATA_FOR_GIVEN_YEAR = 1
     ALL_DATA_FOR_GIVEN_PERIOD = 2
     ALL_DATA_FOR_GIVEN_PERIOD_TOTAL = 3
 
 class Extension():
+    """
+        Filename extensions.
+    """
     DAT = '.dat'
     
 class PlotType():
+    """
+        Different plot type strings.
+    """
     INCOME_VS_EXPENSES = 'income_vs_expenses'
+    
+class ErrorMsg():
+    """
+        Error messages.
+    """
+    CouldNotPrepareLedgerData = 'Error: could not prepare the ledger data: {}'
