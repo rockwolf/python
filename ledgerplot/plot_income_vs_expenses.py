823903f324f9dbb94cd46dbb4e0230cc30d4f47d
rockwolf <thereisanewway@gmail.com>
2015-03-10 16:13:48 +0100

    Refactor

--- ledgerplot/plot_income_vs_expenses.py
+++ ledgerplot/plot_income_vs_expenses.py
@@ -12,8 +12,20 @@ import matplotlib.pyplot as plt
 from decimal import Decimal
 import sys
 from subprocess import call
-from constant import *
+from constant import PlotRetrievalType, Extension, PlotType, ErrMsg, Color
 
+class Const():
+    """
+        Local constants, used in the class of this file.
+    """
+    EXPENSES = 'Expenses'
+    INCOME = 'Income'
+    PROFIT = 'Profit'
+    BAR_WIDTH = 0.15
+    TITLE_YEAR = 'Income vs. expenses - {}'
+    TITLE_DETAIL = 'Income vs. expenses  - per month for period from {} until {}'
+    TITLE_NO_DETAIL = 'Income vs. expenses  - total for period from {} until {}'
+    
 class PlotIncomeVsExpenses():
     """
         Plot income vs expenses.
@@ -27,40 +39,40 @@ class PlotIncomeVsExpenses():
         self.profit = []
         self.dat_file = ''
 
-    def prepare_data(self, a_ledger_file, a_year, a_start_date, a_end_date, a_plot_sub_type = PlotIncomeVsExpensesType.ALL_DATA_UNTIL_NOW):
+    def prepare_data(self, a_ledger_file, a_year, a_start_date, a_end_date, a_plot_sub_type = PlotDataRetrievalType.ALL_DATA_UNTIL_NOW):
         """
             Extract the data we want to plot from ledger.
         """
         try:
             l_file = '{}{}'.format(PlotType.INCOME_VS_EXPENSES, Extension.DAT)
-            if plot_sub_type == PlotIncomeVsExpensesType.ALL_DATA_FOR_GIVEN_PERIOD_TOTAL:
+            if plot_sub_type == PlotDataRetrievalType.ALL_DATA_FOR_GIVEN_PERIOD_TOTAL:
                 call([
                     'ledger', '-f', a_ledger_file,
                     '--real', '-s', '-d', 'T&l<=1',
                     '--begin', a_start_date, '--end', a_end_date,
                     'bal', '-Equity', '-^assets', 'expenses', 'income', '>', l_file
                     ])
-            elif plot_sub_type == PlotIncomeVsExpensesType.ALL_DATA_FOR_GIVEN_PERIOD:
+            elif plot_sub_type == PlotDataRetrievalType.ALL_DATA_FOR_GIVEN_PERIOD:
                 call([
                     'ledger', '-f', a_ledger_file,
                     '--real', '-s', '-d', 'T&l<=1',
                     '--begin', a_start_date, '--end', a_end_date,
                     'bal', '--period-sort', '--monthly', '-Equity', '-^assets', 'expenses', 'income', '>', l_file
                     ])
-            elif plot_sub_type == PlotIncomeVsExpensesType.ALL_DATA_FOR_GIVEN_YEAR:
+            elif plot_sub_type == PlotDataRetrievalType.ALL_DATA_FOR_GIVEN_YEAR:
                 call([
                     'ledger', '-f', a_ledger_file,
                     '--real', '-s', '-p', a_year, '-d', 'T&l<=1',
                     'bal', '--period-sort', '--yearly', '-Equity', '-^assets', 'expenses', 'income', '>', l_file
                     ])
-            elif plot_sub_type == PlotIncomeVsExpensesType.ALL_DATA_UNTIL_NOW:
+            elif plot_sub_type == PlotDataRetrievalType.ALL_DATA_UNTIL_NOW:
                 call([
                     'ledger', '-f', a_ledger_file,
                     '--real', '-s', '-d', 'T&l<=1',
                     'bal', '-Equity', '-^assets', 'expenses', 'income >', a_l_file
                     ])
         except Exception as ex:
-            print 'Error: could not prepare the ledger data: {}'.format(ex)
+            print ErrorMsg.CouldNotPrepareLedgerData.format(ex)
         
     def load_data(self):
         """
@@ -99,24 +111,24 @@ class PlotIncomeVsExpenses():
         l_expenses = self.x_array[1]
 
         l_ind = np.arange(l_N)  # the x locations for the groups
-        l_width = 0.15       # the width of the bars
+        l_width = Const.BAR_WIDTH       # the width of the bars
         l_fig, l_ax = plt.subplots()
-        l_rects1 = ax.bar(l_ind, l_expenses, l_width, l_color = 'r')
-        l_rects2 = ax.bar(l_ind + l_width, l_income, l_width, l_color = 'g')
-        l_rects3 = ax.bar(l_ind + 2 * l_width, (l_expenses - l_income), l_width, l_color = 'b')
+        l_rects1 = ax.bar(l_ind, l_expenses, l_width, l_color = Const.RED)
+        l_rects2 = ax.bar(l_ind + l_width, l_income, l_width, l_color = Const.GREEN)
+        l_rects3 = ax.bar(l_ind + 2 * l_width, (l_expenses - l_income), l_width, l_color = Const.BLUE)
         # add some labels/text
         ax.set_ylabel('Value (EUR)')
         l_title = ''
         if year:
-            l_title = ' - {}'.format(year)
+            l_title = Const.TITLE_YEAR.format(year)
         elif a_is_detail:
-            l_title = ' - per month for period from {} until {}'.format(a_start_date, a_end_date)
+            l_title = Const.TITLE_DETAIL.format(a_start_date, a_end_date)
         elif not a_is_detail:
-            ' - total for period from {} until {}'.format(a_start_date, a_end_date)
-        ax.set_title('Income vs. expenses{}'.format(l_title))
+            Const.TITLE_NO_DETAIL.format(a_start_date, a_end_date)
+        ax.set_title(l_title)
         ax.set_xticks(l_ind + l_width)
         #ax.set_xticklabels( ('2014') ) #TODO: use the year from the input? But do we need this?
-        ax.legend((l_rects1[0], l_rects2[0], l_rects3[0]), ('Expenses', 'Income', 'Profit'))
+        ax.legend((l_rects1[0], l_rects2[0], l_rects3[0]), (Const.Expenses, Const.Income, Const.Profit))
         autolabel(l_rects1)
         autolabel(l_rects2)
         autolabel(l_rects3)
