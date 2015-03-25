578c9400827fcc3bec46865fdcd2b077cee4548c
rockwolf <thereisanewway@gmail.com>
2015-03-26 00:09:18 +0100

    Added extra output.

--- testrest/testrest.py
+++ testrest/testrest.py
@@ -20,12 +20,15 @@ def test_url(a_url):
     r = requests.post(a_url)
     print("Rest api server data:", r.headers)
     print("Data returned by api:", r.text)
+    print("Url from data:", r.url)
+    print("Encoding:", r.encoding)
+    print("Status-code:", r.status_code)
 
 
 if __name__ == "__main__":
     args = docopt(__doc__, help=True, version=__version__)
 
     if args['<url-with-parms>']:
-        print("Url entered:", args['<url-with-parms>'])
+        print("Url entered:", args['<url-with-parms>'].strip())
         test_url(args['<url-with-parms>'].strip())
         
