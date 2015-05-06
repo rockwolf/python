1cc7d9df9675770c483be7572211636341bd143c
rockwolf <thereisanewway@gmail.com>
2015-05-06 09:24:04 +0200

    Update run.py
    Hello world.

--- fade/run.py
+++ fade/run.py
@@ -1,1 +1,11 @@
+from flask import Flask, render_template
+app = Flask(__name__)
 
+@app.route('/')
+def hello_world():
+    l_user = 'admin'
+    l_message = 'Welcome!'
+    return render_template('index.tpl', p_user = l_user, p_message = l_message)
+
+if __name__ == '__main__':
+    app.run()
