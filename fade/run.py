d60c07df04f26ce65e400d91e7f9ad813c426bf0
rockwolf <thereisanewway@gmail.com>
2015-05-06 14:42:04 +0200

    hello_world -> render_home

--- fade/run.py
+++ fade/run.py
@@ -2,7 +2,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+@app.route('/home')
+@app.route('/home/')
+def render_home():
+    """
+        Renders the index page.
+    """
     l_user = 'admin'
     l_message = 'Welcome!'
     return render_template('index.tpl', p_user = l_user, p_message = l_message)
