c5695ade7a63a3ceae2e73f169b8fff924a1449f
rockwolf <thereisanewway@gmail.com>
2015-05-06 09:32:49 +0200

    Adjust system_path.

--- fade/run.py
+++ fade/run.py
@@ -7,5 +7,18 @@ def hello_world():
     l_message = 'Welcome!'
     return render_template('index.tpl', p_user = l_user, p_message = l_message)
 
+def adjust_system_path():
+        """
+            Adjust the system path, so we can search in custom dirs for modules.
+        """
+        sys.path.append('fade')
+        sys.path.append('fade/static')
+        sys.path.append('fade/static/img')
+        sys.path.append('fade/static/js')
+        sys.path.append('fade/static/css')
+        sys.path.append('fade/templates')
+        sys.path.append('instance')
+        
 if __name__ == '__main__':
+    adjust_system_path()
     app.run()
