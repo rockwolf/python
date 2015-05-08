eb4489150705cc92869645deb0b0038d0cd0828d
rockwolf <thereisanewway@gmail.com>
2015-05-08 10:50:48 +0200

    Added form for leveraged contracts.

--- fade/fade/forms.py
+++ fade/fade/forms.py
@@ -1,1 +1,6 @@
+from flask.ext.wtf import Form
+from wtforms import IntegerField
+from wtforms.validators import DataRequired
 
+class FormLeveragedContracts(Form):
+    l_contracts = IntegerField('contracts', validators=[DataRequired()], default=1)
