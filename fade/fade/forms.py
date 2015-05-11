acb8efeeaa7eeeb0e80fb3f3ac0f8c9b0a8f2764
rockwolf <thereisanewway@gmail.com>
2015-05-11 10:38:23 +0200

    l_contracts -> p_contracts

--- fade/fade/forms.py
+++ fade/fade/forms.py
@@ -3,4 +3,4 @@ from wtforms import IntegerField
 from wtforms.validators import DataRequired
 
 class FormLeveragedContracts(Form):
-    l_contracts = IntegerField('contracts', validators=[DataRequired()], default=1)
+    p_contracts = IntegerField('contracts', validators=[DataRequired()], default=1)
