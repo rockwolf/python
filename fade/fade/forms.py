02e10c481274ee652aa16e89739455626edccb51
rockwolf <thereisanewway@gmail.com>
2015-06-02 13:43:55 +0200

    FormTradeJournal

--- fade/fade/forms.py
+++ fade/fade/forms.py
@@ -4,3 +4,7 @@ from wtforms.validators import DataRequired
 
 class FormLeveragedContracts(Form):
     p_contracts = IntegerField('contracts', validators=[DataRequired()], default=1)
+
+class FormTradingJournal(Form):
+    p_trade_id = IntegerField('trade_id', validators=[DataRequired()], default=1)
+    # TODO: needs more fields
