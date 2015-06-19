d5d97781b5426b80c1de977df4cd7fd20882d2d8
rockwolf <thereisanewway@gmail.com>
2015-06-19 08:57:01 +0200

    Update and rename fade/fade/forms.py to fade/src/forms.py

--- fade/src/forms.py
+++ fade/src/forms.py
@@ -1,10 +1,22 @@
+#!/usr/bin/env python
+"""
+    See LICENSE.txt file for copyright and license details.
+"""
 from flask.ext.wtf import Form
 from wtforms import IntegerField
 from wtforms.validators import DataRequired
 
+
 class FormLeveragedContracts(Form):
+    """
+        FormLeveragedContracts
+    """
     p_contracts = IntegerField('contracts', validators=[DataRequired()], default=1)
 
+
 class FormTradingJournal(Form):
+    """
+        FormTradingJournal
+    """
     p_trade_id = IntegerField('trade_id', validators=[DataRequired()], default=1)
     # TODO: needs more fields
