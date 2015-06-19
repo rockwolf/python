c541e85f8b1dccaabd047027e89791d807550ee5
rockwolf <thereisanewway@gmail.com>
2015-06-19 12:02:05 +0200

    Switch database connection string to pg

--- fade/config.py
+++ fade/config.py
@@ -10,6 +10,16 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 WTF_CSRF_ENABLED = True
 SECRET_KEY = '3124534675689780'
 
-# TODO: switch this to postgresql
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+dbuser = 'rockwolf'
+dbpass = ''
+dbhost = 'testdb'
+dbname = 'finance'
+SQLALCHEMY_DATABASE_URI = 'postgresql://'
+                + dbuser
+                + ':'
+                + dbpass
+                + '@'
+                + dbhost
+                + '/'
+                + dbname
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'database')
