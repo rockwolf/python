0e4bdf0d7ec23c8b4bc56c501f2562a74fb9bcbc
rockwolf <thereisanewway@gmail.com>
2015-09-09 11:49:18 +0200

    Code updated.

--- fade/src/views/control_panel.py
+++ fade/src/views/control_panel.py
@@ -18,27 +18,28 @@ def render_account(account_id = None):
     """
         Renders the account page.
     """
-    l_accounts = TAccount.query.filter_by(is_active=1).all()
     l_form = FormAccount(request.form)
+    l_accounts = TAccount.query.filter_by(is_active=1).all()
     l_accounts_total = TAccount.query.count()
     l_accounts_distinct = db.session.query(distinct(TAccount.name)).count()
     l_accounts_has_double = (l_accounts_total != l_accounts_distinct)
     if request.method == "POST":
+        if l_form.account_id.data:
         l_account = TAccount.query.get_or_404(account_id)
+        # or: l_account = TAccount.query.get(l_form.account_id.data)
         l_form = FormAccount(request.form, obj=l_account)
-
-    if l_form.validate_on_submit():
-        #l_account = request.form['account_id']
-        l_form.populate_obj(l_account)
-        return render_template(
-            'control_panel/account.tpl',
-            p_form = l_form,
-            p_accounts = l_accounts,
-            p_accounts_total = l_accounts_total,
-            p_accounts_distinct = l_accounts_distinct,
-            p_accounts_has_double = l_accounts_has_double)
-    else:
-        print '======== form not validated - account_id = {}'.format(account_id)
+        if l_form.validate_on_submit():
+            #l_account = request.form['account_id']
+            l_form.populate_obj(l_account)
+            return render_template(
+                'control_panel/account.tpl',
+                p_form = l_form,
+                p_accounts = l_accounts,
+                p_accounts_total = l_accounts_total,
+                p_accounts_distinct = l_accounts_distinct,
+                p_accounts_has_double = l_accounts_has_double)
+        else:
+            print '======== form not validated - account_id = {}'.format(account_id)
     return render_template(
         'control_panel/account.tpl',
         p_form = l_form,
