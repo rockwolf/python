b202f07ced81f9cb7989a577557aedc84e79886c
rockwolf <thereisanewway@gmail.com>
2015-12-02 16:54:04 +0100

    Update pump.py

--- ledgerpump/src/modules/pump.py
+++ ledgerpump/src/modules/pump.py
@@ -5,18 +5,19 @@
 """
 
 #from decimal import Decimal
-#import sys
-#from subprocess import call
+import sys
+from subprocess import call
+from modules.config import ConfigParser
 
 class Pump():
     """
       Methods related to pumping ledger data to the database.
     """
-  def __init__(self):
+  def __init__(self, a_config):
       """
           Init
       """
-      pass
+      self.config = a_config
   
   def ledger_to_csv(self, a_file):
       """
@@ -24,7 +25,15 @@ class Pump():
       """
       # TODO: get output file (tmp)
       # TODO: export data (ledger command in config)
-      pass
+      l_csv = ''
+      #l_csv = Tempfile.new('ledger')
+      print "Dumping ledger [{0}] to file [{1}]...".format(a_file, l_csv)
+      #system "#{ledger_bin_path} -f #{ledger_file} --format='#{ledger_format}' reg > #{file.path}"
+      call([self.config.cmd_ledger_to_csv.format(self.config.cmd_ledger_bin, a_file, self.config.fmt_ledger_to_csv, l_csv)])
+      #replaced_file = Tempfile.new('ledger')
+      #replaced_file.write(file.read.gsub('\"', '""'))
+      #replaced_file.flush
+      print "Done."
       
   def csv_to_db(self, a_csv):
      """
